70aa6e6965cdc59a45891cbae6e2bf829ae8b821
Jakub <jakub@cassia>
2016-04-14 13:39:12 +0200

    Add delete to plans

--- imports/ui/plans.js
+++ imports/ui/plans.js
@@ -4,7 +4,32 @@ import { Plans } from '../api/plans.js'
 
 Template.plans.helpers({
   plans() {
-    return Plans.find({});
+    return Plans.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.plans.events({
+  'submit .new-plan'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const name = target.planName.value;
+
+    // Insert a task into the collection
+    Plans.insert({
+      //using Mongo.ObjectID to override the default Meteor string IDs.
+      _id: new Mongo.ObjectID,
+      name: name,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.planName.value = '';
+  },
+  'click .delete'() {
+     Plans.remove(this._id);
   },
 });
 

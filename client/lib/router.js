e7f38b92cf5c5b41304fece900422627af0e5d16
Jakub <jakub@cassia>
2016-04-15 14:22:21 +0200

    Minor cleanup

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,7 +1,6 @@
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../imports/ui/plans.js';
-//import '../../imports/ui/plan.js';
 import '../../imports/ui/tasks.js';
 
 FlowRouter.route('/plans', {
@@ -21,21 +20,3 @@ FlowRouter.route('/plans/:id', {
     BlazeLayout.render('mainTemplate', { main: "tasks" });
   },
 });
-
-// this won't be used, itreturns all tasks regardless of the plan : 
-/*
-FlowRouter.route('/tasks', {
-  name: 'Tasks.show',
-  action() {
-  	console.log('Router action for path /tasks');
-    BlazeLayout.render('mainTemplate', { main: "tasks" });
-  },
-});
-
-FlowRouter.route('/about', {
-    action: function(params, queryParams) {
-    	console.log('Router action for path /about');
-        BlazeLayout.render('mainTemplate', { main: "about" });
-    }
-});
-*/
--- imports/ui/tasks.js
+++ imports/ui/tasks.js
@@ -4,11 +4,13 @@ import { Plans } from '../api/plans.js';
 import { Tasks } from '../api/tasks.js';
 
 Template.tasks.helpers({
+  // return the plan itself
   plan: function() {
     var planId = FlowRouter.getParam("id");
     var plan = Plans.findOne({ _id: planId }) || {};
     return plan;
   },
+  // return the tasks of the plan
   tasks: function() {
   	var planId = FlowRouter.getParam("id");
   	console.log('inside tasks function. planId: ', planId);

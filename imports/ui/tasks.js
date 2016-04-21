ead9decdebac3a5a2a00de83c87dd6ac476aef85
Jakub <jakub@cassia>
2016-04-21 09:59:57 +0200

    Add create task functionality

--- imports/ui/tasks.js
+++ imports/ui/tasks.js
@@ -17,5 +17,31 @@ Template.tasks.helpers({
   	tasks = Tasks.find({ planId: planId }) || {};
   	console.log('inside tasks function. tasks.count(): ', tasks.count());
     return tasks;
-  }
+  },
+});
+
+
+Template.tasks.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.taskText.value;
+    const planId = FlowRouter.getParam("id");
+
+    // Insert a task into the collection
+    Tasks.insert({
+      planId: planId,
+      text: text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.taskText.value = '';
+  },
+  'click .delete'() {
+     Tasks.remove(this._id);
+  },
 });

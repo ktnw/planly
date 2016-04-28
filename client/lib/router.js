a562bfae2bdeab2293d0a2100f0b48e04f5a63b1
Jakub <jakub@cassia>
2016-04-28 10:47:12 +0200

    Implement login/logout/account management navigation structure

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../imports/ui/plans.js';
@@ -5,20 +6,46 @@ import '../../imports/ui/planLines.js';
 import '../../imports/ui/plan.js';
 import '../../imports/ui/taskLines.js';
 
+
+
+FlowRouter.route('/', {
+  name: 'Home.show',
+  action() {
+  	console.log('Router action for path: ', FlowRouter.current().path);
+    BlazeLayout.render('main', { app: "home" });
+  },
+});
+
 FlowRouter.route('/plans', {
   name: 'Plans.show',
   action() {
-  	console.log('Router action for path /plans');
-    BlazeLayout.render('mainTemplate', { main: "plans" });
+  	console.log('Router action for path: ', FlowRouter.current().path);
+    BlazeLayout.render('main', { app: "plans" });
   },
 });
 
 FlowRouter.route('/plans/:id', {
   name: 'Plan.show',
-  action(params, queryParams) {
-  	console.log('Router action for path /plans/:id');
-  	console.log('Params:', params);
-  	console.log('queryParams:', queryParams);
-    BlazeLayout.render('mainTemplate', { main: "plan" });
+  action(params) {
+  	console.log('Router action for path: ', FlowRouter.current().path);
+    BlazeLayout.render('main', { app: "plan" });
+  },
+});
+
+FlowRouter.route('/profile', {
+  name: "Profile.show",
+  action() {
+  	console.log('Router action for path: ', FlowRouter.current().path);
+    BlazeLayout.render('main', { app: "profile" });
+  },
+});
+
+FlowRouter.route('/logout', {
+  name: "Logout",
+  action() {
+  	console.log('Router action for path: ', FlowRouter.current().path);
+    Meteor.logout(function() {
+      FlowRouter.go('/');
+    });
   },
 });
--- client/main.js
+++ client/main.js
@@ -1,2 +1,51 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+
+Template.main.onCreated(function(){
+  this.account = new ReactiveVar("login");
+  this.errorMessage = new ReactiveVar();
+});
+
+Template.main.helpers({
+  data: function() {
+    return { "errorMessage": Template.instance().errorMessage.get() };
+  },
+  account: function() {
+  	return Template.instance().account.get();
+  },
+  authInProcess: function() {
+    return Meteor.loggingIn();
+  },
+  authenticated: function() {
+    return !!Meteor.user();
+  }
+});
+
+Template.main.events({
+  'click .register'(event, template) {
+  	template.account.set("register");
+  },
+  'click .forgot-password'(event, template) {
+  	template.account.set("forgot-password");
+  },
+  'click .login'(event, template) {
+  	template.account.set("login");
+  },
+  'click .change-password'(event, template) {
+  	template.account.set("change-password");
+  },
+  'submit .login-form'(event, template) {
+    event.preventDefault();
+    const email = event.target.loginEmail.value;
+    const password = event.target.loginPassword.value;
+      
+    Meteor.loginWithPassword(email,password,function(err){
+      if(err) {
+      	template.errorMessage.set(err.reason);
+      	console.log(err.reason);
+      } else { console.log("Successfully logged in.") }
+    });
+    return false;
+  },
+});
